60081f9e062cbb20662d2cc77d4833f0f7165105
OmniGeek <31792068+OmniGeek@users.noreply.github.com>
2017-09-16 23:07:46 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var count = $('.triangle').length;
+var spin = function(index){
+  
+  var $spinner = $('.spinner');
+  
+  var value = (index>=0)? index : parseInt(Math.random()*count);
+  
+  var preffix = 'index-';
+  
+  $spinner.toggleClass('spin');
+  $spinner[0].className = $spinner[0].className.replace(new RegExp( '(^|\\s)'+preffix+'\\S+','g'), '');
+  $spinner.addClass(preffix+value);
+  
+}
